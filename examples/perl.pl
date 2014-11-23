871e1b617cbb06bce241b4bbf4d5a4169c1dec44
Daniel Perrett <perrettdl@googlemail.com>
2014-11-23 16:04:08 +0000

    more keywords; more fixes to s/// tr/// y///

--- examples/perl.pl
+++ examples/perl.pl
@@ -32,9 +32,18 @@ die q!No $^W! unless $^W;
 {
 	package Acme::Misc;#comments can appear after lines
 	use Data::Dumper 0.2;
-	print Dumper \%-;
+	print Dumper \%-; # todo: it is tricky to distinguish between escapes and references
 	1;
 }
+
+my %numbers = (
+	one   =>    1,
+	two   =>    2.0,
+	three => 0b11,
+	four  =>    4e0,
+	five  =>    5.0e0,
+	ten   => 0x0a,
+);
 
 sub salami_slice ($) {
 	my $recursive;
@@ -46,19 +55,21 @@ sub salami_slice ($) {
 
 my $bindir = FindBin::again();
 
-ITEM: foreach my $item (qw{ $these %words @are not interpolated }) {
-	local $_ = "$item\n";
+ITEM: foreach my $item (sort { fc $a cmp fc $b } map {"$_..."} qw{ $these %words @are not interpolated }) {
+	local $_ = lcfirst "$item\n";
 	s/[\$\%\@]//g; # these lines do
 	y{$%@}    {}d; # the same thing
 	tr{$%@}{   };  # this does not
+	s~\cK~~g;      # something different again
 
 	print qq(\s) for 1..10;
 	print qq(\s) x 10;
-	print ${^MATCH} if /\x74/p;
+	print uc ${^MATCH} if /\x74|\N{LATIN LETTER T}|\x{0074}/p;
 }
 
 print 'Escapes like \n are not interpolated in single quotes' =~ tr/\s/ /; # but they are in transliterations
 
+# todo: heredocs are really hard
 say <<HEREDOC;
 
 it's a 
